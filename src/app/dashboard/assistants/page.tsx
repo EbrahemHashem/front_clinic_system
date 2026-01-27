447e9f6d439fcfa2ea8d6d2cb3bd4403e21db571
Ebrahem Hashem <ebrahemhashem72@gmail.com>
2026-01-27 15:20:55 +0200

    feat: Implement staff management for doctors and assistants with dedicated pages and CRUD operations.

---
+++ src/app/dashboard/assistants/page.tsx
@@ -0,0 +1,9 @@
+import StaffSection from "@/components/staff_section";
+
+export default function AssistantsPage() {
+  return (
+    <div className="p-2">
+      <StaffSection type="assistant" />
+    </div>
+  );
+}
---
+++ src/app/dashboard/doctors/page.tsx
@@ -0,0 +1,9 @@
+import StaffSection from "@/components/staff_section";
+
+export default function DoctorsPage() {
+  return (
+    <div className="p-2">
+      <StaffSection type="doctor" />
+    </div>
+  );
+}
